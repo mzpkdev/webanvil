7528066128d3b78a4c57894b0b291ac4697f8798
Mateusz Pietrzak <mateusz@mzpk.dev>
2026-07-22 11:21:51 +0200

    Configure Webanvil toolchain

---
+++ build.config.ts
@@ -0,0 +1,12 @@
+import { defineBuildConfig } from 'obuild/config'
+
+export default defineBuildConfig({
+  entries: [
+    {
+      type: 'bundle',
+      input: ['./src/index.ts', './src/cli.ts'],
+      outDir: './dist',
+      dts: {},
+    },
+  ],
+})
